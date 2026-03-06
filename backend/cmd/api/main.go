60292a6121dbff0dade07b6f3717390d1fec5237
Retasusan <kenta9736@gmail.com>
2026-03-06 23:59:40 +0900

    revise http handler

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,18 +5,34 @@ import (
 	"net/http"
 
 	"github.com/Retasusan/colab_backend/internal/db"
+	"github.com/Retasusan/colab_backend/internal/org"
 )
 
 func main() {
-	_, err := db.Open()
+	gdb, err := db.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	orgHandler := org.NewHandler(gdb)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	})
 
+	mux.HandleFunc("/api/orgs", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			orgHandler.ListOrganizations(w, r)
+		case http.MethodPost:
+			orgHandler.CreateOrganization(w, r)
+		default:
+			http.Error(w, "method not allowd", http.StatusMethodNotAllowed)
+		}
+	})
+
 	log.Println("server started :8080")
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
